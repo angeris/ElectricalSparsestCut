ce74609b2037018b7de5bdae22b38be67a3a0ca2
Nikhil Garg <nikhil_garg@outlook.com>
2016-12-10 17:35:27 -0800

    changed p back to logn/n

--- algorithms.py
+++ algorithms.py
@@ -9,7 +9,7 @@ import math
 def generate_graphs_with_constraints(n = 100, k = 2, m = 2):
     if m < k:
         raise Exception('m (number of constraints) less than k (number of clusters)')
-    G = nx.gnp_random_graph(n, 10*math.pow(math.log(n),3)/n) #erdos renyi graph that is probably connected
+    G = nx.gnp_random_graph(n, math.pow(math.log(n),1)/n) #erdos renyi graph that is probably connected
     G = nx.convert_node_labels_to_integers(max(nx.connected_component_subgraphs(G), key=len)) #returns largest connected component
     constraints = {}
     for i,x in enumerate(np.random.choice(len(G), size=m)):
@@ -31,15 +31,11 @@ def max_flow_cut(graph, constraints, k):
 
     graph_copy = graph.copy()
     keys = list(constraints.keys())
-    print (keys)
     cut_edges = nx.algorithms.connectivity.minimum_st_edge_cut(graph, keys[0], keys[1])
-
     partition = {}
     graph_copy.remove_edges_from(cut_edges)
     for con in constraints:
-        print('hi', con)
         for node in nx.algorithms.components.node_connected_component(graph_copy, con):
-            print (node),
             partition[node] = constraints[con]
 
     return partition
