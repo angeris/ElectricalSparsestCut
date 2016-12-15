5010bb30f9bc45541ce83cf9fdcf7946ed6cbd48
Nikhil Garg <nikhil_garg@outlook.com>
2016-12-14 17:59:47 -0800

    created test for all 3

--- tests.py
+++ tests.py
@@ -26,3 +26,18 @@ def test_sdp_partition():
 # test_graph_generation()
 # test_max_flow_cut()
 test_sdp_partition()
+
+def test_all_algorithms():
+    G, constraints = algorithms.generate_graphs_with_constraints(n = 100, m = 2, k = 2)
+    partitions_maxflow = algorithms.max_flow_cut(G, constraints, 2)
+    partitions_sdp = algorithms.sdp_partition(G, constraints, 2)
+    partitions_flowcut = algorithms.flow_cut(G, constraints, 2)
+
+    utilities.draw_partitions(G, partitions_maxflow)
+    plt.savefig('maxflow.png')
+
+    utilities.draw_partitions(G, partitions_sdp)
+    plt.savefig('sdp.png')
+
+    utilities.draw_partitions(G, partitions_flowcut)
+    plt.savefig('flowcut.png')
